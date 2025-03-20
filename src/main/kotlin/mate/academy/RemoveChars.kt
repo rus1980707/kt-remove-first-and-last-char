63af608910692d455328fcc76b2d6544b5d7fe60
rus1980707 <rus1980707@gmail.com>
2025-03-20 18:38:11 +0200

    remove-first-and-last

--- src/main/kotlin/mate/academy/RemoveChars.kt
+++ src/main/kotlin/mate/academy/RemoveChars.kt
@@ -2,5 +2,5 @@ package mate.academy
 
 fun removeChars(str: String): String {
     // Implement this function
-    return ""
+    return if (str.length > 2) str.substring(1, str.length - 1) else ""
 }
